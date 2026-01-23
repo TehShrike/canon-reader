a0f2832e74ea2789dce8fb6cd66b3e5ffec3fc93
Josh Duff <me@JoshDuff.com>
2026-01-23 09:45:18 -0600

    goodbye travis/up

--- travis-ci-deploy.sh
+++
@@ -1,4 +0,0 @@
-echo "sudo chown -R $(whoami) /usr/local/bin:" && sudo chown -R $(whoami) /usr/local/bin \
-&& echo "curl -sf https://up.apex.sh/install | sh:" && curl -sf https://up.apex.sh/install | sh \
-&& echo "up upgrade:" && up upgrade \
-&& echo "up --format=plain deploy $1:" && up --format=plain deploy $1
