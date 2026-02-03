adc4f3edca342e611508e06e62f4174afe5a5194
Josh Duff <me@JoshDuff.com>
2026-02-03 17:48:42 -0600

    oh hey, a mediator

--- client/view/home/home.view.ts
+++ client/view/home/home.view.ts
@@ -1,8 +1,10 @@
 import Home from './Home.svelte'
 import type { State } from '#lib/asr_types.ts'
+import type { TypedMediator } from '#lib/mediator_instance.ts'
 
-export default (_mediator: unknown): State => ({
+export default (mediator: TypedMediator): State => ({
 	name: `main.home`,
 	route: ``,
 	template: Home,
+	resolve: () => Promise.resolve({ mediator }),
 })
